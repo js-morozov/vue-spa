1186767ede84cbe783c628a0a4f9780efa6c2adb
js_morozov <frontend.morozov@gmail.com>
2020-11-29 11:35:44 +0300

    Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="counter">
+    <base-button @click="subNumber">-</base-button>
+    <input type="text" class="counter__input" v-model.number="number" />
+    <base-button @click="addNumber">+</base-button>
+  </div>
+</template>
+
+<script>
+import BaseButton from "./controls/BaseButton";
+
+export default {
+  components: { BaseButton },
+  props: {
+    defaultValue: {
+      type: Number,
+      default: 10,
+    },
+  },
+  data() {
+    return {
+      number: this.defaultValue,
+    };
+  },
+  methods: {
+    addNumber() {
+      this.number = this.number + 1;
+    },
+    subNumber() {
+      this.number = this.number - 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.counter {
+  display: flex;
+  &__button {
+    margin: 0 5px;
+  }
+}
+</style>
---
+++ src/components/controls/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="button" @click="$emit('click', $event)">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border: none;
+  outline: none;
+  background: #1abc9c;
+  color: #ffffff;
+  border-radius: 10px;
+  margin: 5px;
+}
+</style>
